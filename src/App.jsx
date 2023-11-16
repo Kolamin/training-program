7795a59dbbd5c02e902ab5762bb123ec13728879
Kolamin <gofranich@gmail.com>
2023-11-16 10:58:39 +0600

    add select themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,14 @@
 import { useEffect, useState } from "react";
-import { colAllThemes } from "./data";
+
 import { getDocs, query, where } from "firebase/firestore";
 import "./styles/styles.css";
 import Theme from "./components/Theme";
+import { SelectThemes } from "./components/SelectThemes";
 
 function App() {
-  const [themes, setThemes] = useState([]);
-  const [month, setMonth] = useState("Все темы");
-
-  /*  useEffect(() => {
-    const getThemes = async (m) => {
-      if (month === "All_themes") {
-        const data = await getDocs(colAllThemes);
-        setThemes(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-      } else {
-        const q = query(colAllThemes, where("month", "==", m));
-        const data = await getDocs(q);
-        setThemes(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-      }
-    };
-
-    getThemes(month);
-  }, [month]); */
-
-  /*   const getThemes = async () => {
-    const data = await getDocs(colAllThemes);
-    setThemes(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-  }; */
-
-  const getThemes = async (month) => {
-    if (month === "Все темы") {
-      const data = await getDocs(colAllThemes);
-      setThemes(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    } else {
-      const q = query(colAllThemes, where("month", "==", month));
-      const data = await getDocs(q);
-      setThemes(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    }
-  };
-
   return (
     <div>
-      <div className="button">
-        <select value={month} onChange={(e) => setMonth(e.target.value)}>
-          <option value="Все темы">Все темы</option>
-          <option value="Январь">Январь</option>
-          <option value="Февраль">Февраль</option>
-          <option value="Март">Март</option>
-          <option value="Апрель">Апрель</option>
-          <option value="Май">Май</option>
-          <option value="Июнь">Июнь</option>
-          <option value="Июль">Июль</option>
-          <option value="Август">Август</option>
-          <option value="Сентябрь">Сентябрь</option>
-          <option value="Октябрь">Октябрь</option>
-          <option value="Ноябрь">Ноябрь</option>
-          <option value="Декабрь">Декабрь</option>
-        </select>
-        <button onClick={() => getThemes(month)}>Выбрать тему</button>
-      </div>
-
-      <div className="question">
-        <Theme themes={themes} />
-      </div>
+      <SelectThemes />
     </div>
   );
 }
---
+++ src/components/SelectThemes.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { colAllThemes } from "../data";
+import { getDocs, query, where } from "firebase/firestore";
+import "../styles/styles.css";
+import Theme from "./Theme";
+
+export const SelectThemes = () => {
+  const [themes, setThemes] = useState([]);
+  const [month, setMonth] = useState("Все темы");
+
+  const getThemes = async (month) => {
+    if (month === "Все темы") {
+      const data = await getDocs(colAllThemes);
+      setThemes(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    } else {
+      const q = query(colAllThemes, where("month", "==", month));
+      const data = await getDocs(q);
+      setThemes(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    }
+  };
+
+  return (
+    <div>
+      <div className="button">
+        <select value={month} onChange={(e) => setMonth(e.target.value)}>
+          <option value="Все темы">Все темы</option>
+          <option value="Январь">Январь</option>
+          <option value="Февраль">Февраль</option>
+          <option value="Март">Март</option>
+          <option value="Апрель">Апрель</option>
+          <option value="Май">Май</option>
+          <option value="Июнь">Июнь</option>
+          <option value="Июль">Июль</option>
+          <option value="Август">Август</option>
+          <option value="Сентябрь">Сентябрь</option>
+          <option value="Октябрь">Октябрь</option>
+          <option value="Ноябрь">Ноябрь</option>
+          <option value="Декабрь">Декабрь</option>
+        </select>
+        <button onClick={() => getThemes(month)}>Выбрать тему</button>
+      </div>
+
+      <div className="question">
+        <Theme themes={themes} />
+      </div>
+    </div>
+  );
+};
